bc98f8516789a561448bfba69a56dc7bfe46ab16
sarvochcha01 <sarvochcha01@gmail.com>
2024-03-12 21:04:03 +0530

    mobile navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,69 @@
 import { Link, Outlet } from "react-router-dom";
 import logo from "./img/Z-AnonymityLogo.svg";
 
-import { CiMenuBurger } from "react-icons/ci";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
 function App() {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
   return (
-    <div className=" w-screen max-w-screen-2xl h-screen mx-auto ">
+    <div className="flex flex-col w-screen h-screen max-w-screen-2xl mx-auto ">
       <nav className="flex items-center justify-between h-24">
         <Link to="/" className="w-36 absolute left-6">
           <img src={logo} alt="Logo" />
         </Link>
         <div className="hidden lg:flex justify-center items-center w-full gap-24 font-semibold text-lg">
-          <Link to="/home">Home</Link>
+          <Link to="/">Home</Link>
           <Link to="/faq">FAQ</Link>
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
         </div>
-        <CiMenuBurger className="lg:hidden absolute right-6" size={35} />
+        <IoMenu
+          className="lg:hidden absolute right-6"
+          size={35}
+          onClick={() => setIsNavbarOpen((prevState) => !prevState)}
+        />
       </nav>
+
+      {isNavbarOpen && (
+        <div className="absolute top-0 w-full h-[100svh] bg-white flex gap-12 text-2xl font-semibold flex-col justify-center text-center items-center z-50">
+          <IoClose
+            className="lg:hidden absolute right-6"
+            size={35}
+            onClick={() => setIsNavbarOpen((prevState) => !prevState)}
+          />
+          <Link
+            to="/"
+            className="text-center top-20 px-8 py-3"
+            onClick={() => setIsNavbarOpen(false)}
+          >
+            Home
+          </Link>
+          <Link
+            to="/faq"
+            className="text-center top-40 px-8 py-3"
+            onClick={() => setIsNavbarOpen(false)}
+          >
+            FAQ
+          </Link>
+          <Link
+            to="/about"
+            className="text-center -top-40 px-8 py-3"
+            onClick={() => setIsNavbarOpen(false)}
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            className="text-center -top-50 px-8 py-3"
+            onClick={() => setIsNavbarOpen(false)}
+          >
+            Contact
+          </Link>
+        </div>
+      )}
       <Outlet />
     </div>
   );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const Home = () => {
   return (
     <div className="w-full flex justify-center overflow-x-hidden">
-      <span className="h-32 w-64 lg:w-[600px] z-50  bg-hero bg-contain bg-no-repeat bg-center absolute top-60 lg:top-48" />
+      <span className="h-32 w-64 lg:w-[600px] z-0  bg-hero bg-contain bg-no-repeat bg-center absolute top-60 lg:top-48" />
 
-      <span className="h-56 w-36 lg:w-80 bg-hindi bg-contain bg-no-repeat bg-center absolute -left-8 top-16 lg:top-24  lg:-left-20" />
-      <span className="h-56 w-36 lg:w-80   bg-spanish bg-contain bg-no-repeat bg-center absolute -right-10 lg:-right-32" />
-      <span className="h-56  w-36 lg:w-80 bg-japnese bg-contain bg-no-repeat bg-center absolute top-[500px] left-0  lg:top-[450px] lg:left-20" />
-      <span className="h-56 w-36 lg:w-80   bg-russian bg-contain bg-no-repeat bg-center absolute top-[450px]  lg:top-96 right-0" />
+      <span className="h-28 lg:h-56 w-36 lg:w-80 bg-hindi bg-contain bg-no-repeat bg-center absolute -left-8 top-24 lg:top-24  lg:-left-20" />
+      <span className="h-28 lg:h-56  w-36 lg:w-80 bg-japnese bg-contain bg-no-repeat bg-center absolute top-[520px] left-0  lg:top-[450px] lg:left-20" />
+      <span className="h-28 lg:h-56 w-36 lg:w-80   bg-russian bg-contain bg-no-repeat bg-center absolute top-[500px]  lg:top-96 right-0" />
+      <span className="h-28 lg:h-44 w-36 lg:w-80   bg-spanish bg-contain bg-no-repeat bg-center absolute top-32  lg:top-32 -right-10 lg:-right-32" />
 
       <span className="h-96 w-[350px] lg:w-[1000px] bg-trailExtended2 bg-contain bg-no-repeat bg-center absolute top-[206px] lg:top-[210px]" />
 
-      <div className="absolute top-[470px] lg:top-[550px] border-2 bg-white border-black rounded-2xl px-8 py-3 lg:px-12 lg:py-4 cursor-pointer hover:translate-y-1 z-50">
+      <div className="absolute top-[470px] lg:top-[550px] border-2 bg-white border-black rounded-2xl px-8 py-3 lg:px-12 lg:py-4 cursor-pointer hover:translate-y-1 z-10">
         Start texting
       </div>
       <div className="absolute top-[480px] lg:top-[560px] border-2 border-black bg-black rounded-2xl px-8 py-3 lg:px-12 lg:py-4 cursor-pointer z-0">
