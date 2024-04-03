31f77b8f196610aa5cf18f364f4ef8a0e1c7a0f5
Divyansh Yaduvanshi <divyanshyaduvanshi2004@gmail.com>
2024-04-03 14:16:22 +0530

    msgBox animation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <div className="flex  w-full h-[calc(100vh-250px)] justify-center items-center text-xl font-semibold">
+    <div className="flex w-full h-[calc(100vh-250px)] justify-center items-center text-xl font-semibold">
       Hehe, you can’t sawri.
-      <span className="absolute bottom-6 right-4 text-sm">
-        jk here’s our email: zero.anonymity@gmail.com
-      </span>
+      <div className="flex flex-row">
+        <span className="absolute bottom-6 right-[167px] lg:bottom-6 lg:right-52 text-xs lg:text-sm">
+          jk here’s our email:
+        </span>
+        <motion.span
+          className="absolute bottom-6 right-4 lg:bottom-6 lg:right-4 text-xs lg:text-sm"
+          initial={{ opacity: 0 }}
+          whileHover={{ opacity: 1 }}
+          whileTap={{ opacity: 1 }}
+          onTapCancel={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          zero.anonymity@gmail.com
+        </motion.span>
+      </div>
     </div>
   );
 };
