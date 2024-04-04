43153fe34c7e3e43496616117442793a45c951fb
sarvochcha01 <sarvochcha01@gmail.com>
2024-04-04 22:30:33 +0530

    logout button

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,92 @@
 import { Link, Outlet, NavLink } from "react-router-dom";
 import logo from "./img/Z-AnonymityLogo.svg";
 
-import { IoMenu } from "react-icons/io5";
-import { IoClose } from "react-icons/io5";
+import { IoMenu, IoClose } from "react-icons/io5";
+import { FaChevronDown } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
+import { VscChromeClose } from "react-icons/vsc";
 import { useEffect, useState } from "react";
 
 function App() {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
-  
+
+  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
+
   useEffect(() => {
     const handleBeforeUnload = (event) => {
       event.preventDefault();
       console.log("<OKOKOKOK></OKOKOKOK>");
-      const confirmationMessage = 'Are you sure you want to leave this page?';
+      const confirmationMessage = "Are you sure you want to leave this page?";
       event.returnValue = confirmationMessage;
       return confirmationMessage;
     };
 
-    window.addEventListener('beforeunload', handleBeforeUnload);
+    window.addEventListener("beforeunload", handleBeforeUnload);
 
     return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
+      window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, []);
 
   return (
-    <div className="flex flex-col w-screen h-screen max-w-screen-2xl mx-auto ">
-      <nav className=" bg-[#003049] flex items-center justify-between h-20">
+    <div className="flex flex-col w-screen h-screen  mx-auto ">
+      <nav className="  flex items-center justify-between h-20">
         <Link to="/" className="w-36 relative left-6">
           <img src={logo} alt="Logo" />
         </Link>
         <div className="hidden lg:flex justify-center items-center w-full gap-24 font-semibold text-lg ">
           <NavLink
-            className="hover:text-blue-800 text-white border-solid border-gray-400 hover:border-b-2 px-2 "
+            className="hover:text-blue-800 border-solid border-gray-400 hover:border-b-2 px-2 "
             to="/"
           >
             Home
           </NavLink>
           <NavLink
-            className="hover:text-blue-800 text-white border-solid border-gray-400 hover:border-b-2 px-2 "
+            className="hover:text-blue-800 border-solid border-gray-400 hover:border-b-2 px-2 "
             to="/faq"
           >
             FAQ
           </NavLink>
           <NavLink
-            className="hover:text-blue-800 text-white border-solid border-gray-400 hover:border-b-2 px-2 "
+            className="hover:text-blue-800 border-solid border-gray-400 hover:border-b-2 px-2 "
             to="/about"
           >
             About
           </NavLink>
           <NavLink
-            className="hover:text-blue-800 text-white border-solid border-gray-400 hover:border-b-2 px-2 "
+            className="hover:text-blue-800 border-solid border-gray-400 hover:border-b-2 px-2 "
             to="/contact"
           >
             Contact
           </NavLink>
         </div>
-        <div className="bg-white rounded-3xl h-10 w-[90px] px-2 relative right-12 mr-10 flex justify-center items-center gap-3 ">
+        <div className=" rounded-3xl h-10 w-[90px] px-2 relative right-12 mr-10 flex justify-center items-center gap-3 ">
           <img
             src="https://banner2.cleanpng.com/20180523/tha/kisspng-businessperson-computer-icons-avatar-clip-art-lattice-5b0508dc6a3a10.0013931115270566044351.jpg"
             alt="I"
             className="rounded-full h-8 w-8"
           />
-          <img
-            src="https://cdn-icons-png.flaticon.com/128/32/32195.png"
-            alt="I"
-            className="rounded-full h-5 w-5"
-          />
-          </div>
+          {isProfileDropdownOpen ? (
+            <VscChromeClose
+              onClick={() =>
+                setIsProfileDropdownOpen((prevState) => !prevState)
+              }
+            />
+          ) : (
+            <IoIosArrowDown
+              onClick={() =>
+                setIsProfileDropdownOpen((prevState) => !prevState)
+              }
+            />
+          )}
+          {isProfileDropdownOpen && (
+            <div className="w-60 h-24  absolute z-50 top-10 -left-40 bg-gray-200 flex justify-center items-center rounded-3xl">
+              <div className="border-2 w-40 h-12 bg-white border-black rounded-2xl  cursor-pointer flex items-center justify-center ">
+                Log out
+              </div>
+            </div>
+          )}
+        </div>
         <IoMenu
           className="lg:hidden absolute right-6"
           size={35}
@@ -110,7 +129,6 @@ function App() {
           >
             Contact
           </Link>
-          
         </div>
       )}
       <Outlet />
